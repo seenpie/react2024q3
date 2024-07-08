98bf8f6ac5d262d9a5503cf19ea17cf933917a42
fatsharkyes <bananananasez@gmail.com>
2024-07-08 18:09:29 +0300

    feat: add Layout and change styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import ErrorBoundary from "./components/ErrorBaundary.tsx";
 import Header from "./components/Header/Header.tsx";
 import Main from "./components/Main/Main.tsx";
 import PokemonProvider from "./context/PokemonProvider.tsx";
+import Layout from "./components/Layout/Layout.tsx";
 
 class App extends Component {
   render() {
     return (
       <ErrorBoundary>
         <PokemonProvider>
-          <Header />
-          <Main />
+          <Layout>
+            <Header />
+            <Main />
+          </Layout>
         </PokemonProvider>
       </ErrorBoundary>
     );
