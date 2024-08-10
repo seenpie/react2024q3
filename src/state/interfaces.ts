93125d71c362e742011e1ab805fce67c3641d510
seenpie <bananananasez@gmail.com>
2024-08-10 20:10:35 +0300

    refactor: remove pageDataSlice and change state for nextjs app

--- src/state/interfaces.ts
+++ src/state/interfaces.ts
@@ -4,3 +4,24 @@ export interface IResourceList extends PokeAPI.NamedAPIResourceList {}
 export interface IResource extends PokeAPI.NamedAPIResource {}
 export interface IPokemonSpecies extends PokeAPI.PokemonSpecies {}
 export interface IPokemon extends PokeAPI.Pokemon {}
+
+export interface IPageParams {
+  totalItems: number;
+  offset: number;
+  limit: number;
+}
+
+export interface ISelectedItemData {
+  name: string;
+  height: number;
+  weight: number;
+  type: string;
+  experience: number;
+  image: string;
+}
+
+export interface IPageData {
+  itemList: IResource[];
+  pageParams: IPageParams;
+  selectedItem: ISelectedItemData | null;
+}
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,13 +1,12 @@
+"use client";
+
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { favoritePokemonListSlice } from "./favoritePokemonList/favoritePokemonListSlice.ts";
-import { pageDataSlice } from "./pageData/pageDataSlice.ts";
 import { pokemonApi } from "./pokemonApi.ts";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { createWrapper } from "next-redux-wrapper";
 
 const rootReducer = combineReducers({
   favoritePokemonList: favoritePokemonListSlice.reducer,
-  pageData: pageDataSlice.reducer,
   [pokemonApi.reducerPath]: pokemonApi.reducer
 });
 
@@ -24,5 +23,3 @@ export const makeStore = () => {
 export type AppState = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppState["getState"]>;
 export type AppDispatch = AppState["dispatch"];
-
-export const wrapper = createWrapper<AppState>(makeStore);
